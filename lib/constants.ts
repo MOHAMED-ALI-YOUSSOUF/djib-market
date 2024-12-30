b1b50295d43e6fdb2550c5a415d7f8b5b464516f
MOHAMED-ALI-YOUSSOUF <wizzimed@gmail.com>
2024-12-30 21:48:28 +0300

    productDetails page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
 export const APP_NAME= process.env.NEXT_PUBLIC_APP_NAME || 'Djib-market'
 export const APP_SLOGAN= process.env.NEXT_PUBLIC_APPSLOGAN || 'Spend less, enjoy more'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Djib-market is a marketplace where you can buy and sell products easily and quickly, with a wide range of categories and a user-friendly interface.'
+
+export const PAGE_SIZE= Number(process.env.PAGE_SIZE || 9)
